cfe1893710731c8ffea750820caa61808ef8db57
Li Qian <ericqianli@gmail.com>
2020-04-20 02:28:29 -0700

    Updates

---
+++ precache-manifest.6d9e024301b9d180dba982b4dbf96779.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9158818e3ff97c6d4f29ef84b37a97",
+    "url": "/tianyahaige/index.html"
+  },
+  {
+    "revision": "0f419d9c5310bda1383d",
+    "url": "/tianyahaige/static/css/main.bcd146c3.chunk.css"
+  },
+  {
+    "revision": "278640e26ce589b43518",
+    "url": "/tianyahaige/static/js/2.585569fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tianyahaige/static/js/2.585569fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f419d9c5310bda1383d",
+    "url": "/tianyahaige/static/js/main.237850af.chunk.js"
+  },
+  {
+    "revision": "625ec1c6646c5666471f",
+    "url": "/tianyahaige/static/js/runtime-main.f6132870.js"
+  },
+  {
+    "revision": "62a993150010db1065187112d6c173c2",
+    "url": "/tianyahaige/static/media/background_body.62a99315.jpg"
+  },
+  {
+    "revision": "50d23bf81e9b3eed92a1578b46dd7961",
+    "url": "/tianyahaige/static/media/background_left.50d23bf8.jpg"
+  },
+  {
+    "revision": "bc21eb1bff92dc8ce8943474b6bed4d9",
+    "url": "/tianyahaige/static/media/background_right.bc21eb1b.jpg"
+  }
+]);
