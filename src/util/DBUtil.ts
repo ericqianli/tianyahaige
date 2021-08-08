7cc715962e7c321dcb919f85fc4aaffd5be367b5
Li Qian <Everdise@Lis-iMac.local>
2021-08-07 18:41:51 -0700

    use Durstenfeld shuffle

--- src/util/DBUtil.ts
+++ src/util/DBUtil.ts
@@ -12,20 +12,39 @@ const DBUtil = {
         );
     },
 
+    // Implements Durstenfeld shuffle, an optimized version of Fisher-Yates.
+    shuffle(array: any[]) {
+        for (var i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    },
+
     fetchPoemsByCollection(collection: number, sampled: boolean): Promise<any> {
         const poems: any[] = (poemData as any).default;
-
+        const collectionPoems = poems.filter((poem) =>
+            poem.collections.includes(collection)
+        );
         return new Promise((resolve) => {
-            const collectionPoems = Immutable.List(poems)
-                .map((poem: any) => poem as Poem)
-                .filter((poem) => poem.collections.includes(collection));
             if (sampled) {
-                resolve(collectionPoems
-                    .sort(() => 0.5 - Math.random())
+                this.shuffle(collectionPoems);
+                const selectedPoems = collectionPoems
                     .slice(0, 18)
-                    .sort((p1, p2) => p1.id - p2.id));
+                    .sort((p1, p2) => p1.id - p2.id);
+                resolve(
+                    Immutable.List(selectedPoems).map(
+                        (poem: any) => poem as Poem
+                    )
+                );
+            } else {
+                resolve(
+                    Immutable.List(collectionPoems).map(
+                        (poem: any) => poem as Poem
+                    )
+                );
             }
-            resolve(collectionPoems);
         });
     },
 };
